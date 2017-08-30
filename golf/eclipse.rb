0bb52165e43056cd5c1db17af30840e5420a13f9
Andrew McLees <andrewmclees5@gmail.com>
2017-08-30 14:14:43 -0700

    Add brackets lambda

---
+++ golf/eclipse.rb
@@ -0,0 +1,16 @@
+eclipse = lambda { |n|
+  out = ''
+  0..6.each do |step|
+    half = case step
+      when
+
+    out << half << middle << half << "\n"
+  end
+  out
+}
+
+puts eclipse.call(0)
+puts eclipse.call(1)
+puts eclipse.call(2)
+puts eclipse.call(3)
+puts eclipse.call(4)
